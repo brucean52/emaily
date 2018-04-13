964beb3e6cf7257dad2371d2319219e2a560e441
brucean52 <bruce.an52@gmail.com>
2018-04-12 17:15:21 -0700

    google oauth complete

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-        }, (accessToken)=> {
-            console.log(accessToken);
-        })
+        }, 
+            (accessToken, refreshToken, profile, done)=> {
+                console.log('acccessToken', accessToken);
+                console.log('refreshToken', refreshToken);
+                console.log('profile', profile);
+            }
+        )
 );
 // app.get('/', (req, res)=>{
 //     res.send({ bye: 'buddy'});
 // });
+
+//passport has internal identifier 'google'
+//scope - google internal list for what I am requesting
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
