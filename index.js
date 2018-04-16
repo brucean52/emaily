51292dd92179b17d8c645b6c28601c11de57d777
brucean52 <bruce.an52@gmail.com>
2018-04-16 16:13:49 -0700

    added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 authRoutes(app);
@@ -15,4 +19,4 @@ authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT);   
