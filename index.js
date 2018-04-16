9460f445f7f076a7f52674cfa33f6c6d877214ad
brucean52 <bruce.an52@gmail.com>
2018-04-16 15:40:00 -0700

    refactored server code

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+authRoutes(app);
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-        }, 
-            (accessToken, refreshToken, profile, done)=> {
-                console.log('acccessToken', accessToken);
-                console.log('refreshToken', refreshToken);
-                console.log('profile', profile);
-            }
-        )
-);
-// app.get('/', (req, res)=>{
+// app.get('/', (req, res)=>{  
 //     res.send({ bye: 'buddy'});
 // });
 
 //passport has internal identifier 'google'
 //scope - google internal list for what I am requesting
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
 
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
