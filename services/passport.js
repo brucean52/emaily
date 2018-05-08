c4b2986d97cb29534c8b558761d1dc3c3ff747c1
brucean52 <bruce.an52@gmail.com>
2018-05-08 15:38:14 -0700

    google oauth login working

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=> {
+    User.findById(id).then(user => {
+            done(null, user);
+        });
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -19,9 +29,12 @@ passport.use(
                 .then((existingUser)=>{
                     if (existingUser){
                         //existing user exists with profile id
+                        done(null, existingUser);
                     } else {
                         //user record doesn't exists
-                        new User({ googleId: profile.id }).save();
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
                     }
                 })
         }
